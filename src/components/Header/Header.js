e187575475370b7459d8e28a78ffcfd68aad04ae
Adam <adamkayal@live.ca>
2021-09-16 21:08:19 -0400

    header and router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <nav className="header">
+            <img
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amaozon_PNG11.png"
+                alt="logo"
+            />
+        </nav>
+    )
+}
+
+export default Header
